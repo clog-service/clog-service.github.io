54eb7027656810341e3f6191da46015e02313948
dev-yakuza <dev.yakuza@gmail.com>
2020-09-01 09:26:59 +0900

    release

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/login":["static\u002Fchunks\u002Fpages\u002Flogin.js"],"/newspaper":["static\u002Fchunks\u002Fpages\u002Fnewspaper.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
