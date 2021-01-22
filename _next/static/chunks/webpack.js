29b7163262d7e8a9a7bbb115af06f28c4eb75c1c
dev-yakuza <dev.yakuza@gmail.com>
2021-01-22 09:35:34 +0900

    deploy

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "84a0f6e105ddeaebde97";
+/******/ 	var hotCurrentHash = "4f78f44d62202353cc26";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
